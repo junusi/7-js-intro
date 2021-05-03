1a7986cf2467e0bc0dbf6108e5c50ea5b4592def
junusi <82803045+junusi@users.noreply.github.com>
2021-05-03 21:05:31 +0300

    main

---
+++ js/01-variables.js
@@ -0,0 +1,51 @@
+/* kintameji:
+tipai. pagal iniciavimo buda:
+- const (default)
+- let
+- var (nenaudoti)
+pagal informacijos tipa
+- number (skaiciai)
+- stringai (tekstas)
+- boolean (logine reiksme)
+- array (sarasas)
+- object (objektas)
+- nujj
+- undefined
+
+*/
+
+
+const amzius = 99;
+const vardas = 'vardenis';
+const buvoDuse = true;
+const dabarYraNaktis = false;
+const dabarSninga = true;
+const pazymiai = [5,2,10,8,7];
+const draugai = ['ivan','karolis','loreta'];
+const Maryte = {
+    vardas: 'Maryte',
+    amzius: 89,
+    megstamaSpalva : 'green',
+    gyvenamojiVieta: {
+        mestas: 'Palanga',
+        gatve: 'Basanaviciaus',
+        namoNumeris: 1
+    }
+}
+const bendraPazymiuSuma = 0;
+console.log(bendraPazymiuSuma);
+console.log(pazymiai);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
