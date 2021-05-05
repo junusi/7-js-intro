f7052c9a469f1cc589b83765e111d6d7759dfffd
junusi <82803045+junusi@users.noreply.github.com>
2021-05-05 20:37:52 +0300

    terner2

--- js/05-ternary.js
+++ js/05-ternary.js
@@ -20,3 +20,45 @@ console.log('Ar ileisti:', arIleisti);
 
 const arVyresnis = amzius >= cenzas ? ' viresnis ' : ' neviresnis';
 console.log('ar viresnis :' ,arVyresnis);
+//--------------------------------------------------
+const amzius2 = 114;
+const cenzas2 = 18;
+let arIleisti2 = null;
+if (amzius2 >= cenzas2) {
+    arIleisti2 = true;
+} else {
+    arIleisti2 = false;
+}
+console.log('Ar ileisti :', arIleisti2);
+//---------------------------------------------------
+
+const ats = 0 > 4 ? 5 : 7;
+
+const ats = if (0 < 4) {
+                         ? 5
+} else {
+                         :7
+}
+
+console.log(ats);
+
+const ats = 0 < 1 ? 2 < 3 ? 4 : 5 : 6 ;
+/*
+0 < 1
+     ? 2 < 3
+           ? 4
+           :5
+    :6 ;
+*/
+console.log(ats);
+//---------------------------------------------------
+
+console.log(boo);
+
+/*const boo = true 
+                 ? false 
+                       ? true 
+                       : false 
+             : true ;
+
+*/
