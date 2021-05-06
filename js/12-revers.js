0c4135b7dfc04d6b5f73c7c1142303b5916a1e02
junusi <82803045+junusi@users.noreply.github.com>
2021-05-06 08:36:23 +0300

    revers-string

---
+++ js/12-revers.js
@@ -0,0 +1,20 @@
+/*
+
+REVERS STRING
+
+*/
+const tekstas = 'abcdef';
+let revers = '';
+
+for (let i = tekstas.length-1; i >= 0; i--) { revers += tekstas[i];
+    console.log(i,'->',tekstas[i]);
+    
+}
+console.log(tekstas,'->',revers);
+
+
+
+
+
+
+
