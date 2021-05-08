10d35fbdeb68c58e95deed80174543c500cee924
junusi <82803045+junusi@users.noreply.github.com>
2021-05-08 13:44:29 +0300

    slamstas

--- js/codowar.js
+++ js/codowar.js
@@ -10,9 +10,7 @@ Output
 Check to see if a string has the same amount of 'x's and 'o's.
  The method must return a boolean and be case insensitive.
   The string can contain any char.
-
 Examples input/output:
-
 XO("ooxx") => true
 XO("xooxx") => false
 XO("ooxXm") => true
@@ -36,7 +34,7 @@ if    ( a % 2 === 0 )
 }
 
 let r = '';
-const str =("xooxx") ;
+const str =("ppppoooo") ;
 const a = str.length;
 console.log(XO(str));
 console.log('tecsto turinis:',str);
