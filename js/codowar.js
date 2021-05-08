d6996fabed7def25edb5bf600dca17142900a982
junusi <82803045+junusi@users.noreply.github.com>
2021-05-08 12:41:36 +0300

    codowar

---
+++ js/codowar.js
@@ -0,0 +1,43 @@
+/*7 kyu
+Exes and Ohs
+87619688% of 12,74836,808 of 107,329joh_pot1 Issue Reported
+JavaScript
+Node v10.x
+VIM
+EMACS
+Instructions
+Output
+Check to see if a string has the same amount of 'x's and 'o's.
+ The method must return a boolean and be case insensitive.
+  The string can contain any char.
+
+Examples input/output:
+
+XO("ooxx") => true
+XO("xooxx") => false
+XO("ooxXm") => true
+XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+XO("zzoo") => false
+*/
+function XO(str)
+{
+if    ( a % 2 === 0 )
+{ console.log('taip tekstas simetrinis') ;
+    for (let i = 0; i < str.length / 2  ;  i = i + 1)
+{
+    str [i] === str [(str.length / 2) - 1] ? console.log('Pirmos teksto dales  raide:',str [i]) : console.log ('skirtinga raide:',str[i]);}
+  for (let i = str.length /2 ;i < str.length ;i = i + 1)
+{  str [i] === str [str.length -1] ? console.log('Antros teksto dales  raide:',str [i]) : console.log ('skirtinga raide:',str[i]);
+}
+} else
+{console.log('ne tekstas ne simetrinis');  
+}
+    return str.length; 
+}
+
+let r = '';
+const str =("xooxx") ;
+const a = str.length;
+console.log(XO(str));
+console.log('tecsto turinis:',str);
+console.log(`teksto ilgis ${str.length} raides`);
