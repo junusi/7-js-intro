7246fc69afa02cf9698b53acd0e8da3b348ccc9d
junusi <82803045+junusi@users.noreply.github.com>
2021-05-10 17:27:48 +0300

    uzdotis

---
+++ js/18-codowar.js
@@ -0,0 +1,16 @@
+
+
+
+
+function  check(a,x) {
+    for (let i = 0; i < a.length; i = i +1) {
+        console.log(a);
+      const lego = a[i];
+      console.log(lego) ; 
+    }
+}
+
+console.log(check([80,117,115,104,45,85,112,125],45),'->',true);
+
+
+
