09006e1a27e8f978a57f9cdea451c1c07d6f4c02
junusi <82803045+junusi@users.noreply.github.com>
2021-05-27 12:49:15 +0300

    update

--- js/gallon.js
+++ js/gallon.js
@@ -1,26 +1,10 @@
+let sum = 1;
+let a = 1;
+for (let i = 5; i > 0; i = 5 - 1) {
+  a = a * i;
+  sum = sum * a;
 
-//---------------------------------------------------------
-/*Atlikite funkciją, kuri užima du sveikus skaičius (a, b, kur a <b)
- ir grąžina visų sveikų skaičių masyvą tarp įvesties parametrų, įskaitant juos.
-
-Pavyzdžiui:
-
-a = 1
-b = 4
--> [1, 2, 3, 4]*/
-function between(a, b) {
-
-  let c = [];
-  c[0] = a;
-  for(let i = 1;i < (b - a) + 1 ;i = i + 1){
-  c[i] = c[i - 1] + 1;
-    
-  }
-  return c;
 }
-console.log(between(1, 4), [1, 2, 3, 4]);
-console.log(between(-2, 2), [-2, -1, 0, 1, 2]);
 
-
-
+console.log(sum);
 
